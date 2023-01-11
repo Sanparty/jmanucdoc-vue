d91850067217d2ffcd250c4de9886f591400cfb8
Dan San Pedro <dan.sanpedro@bellmedia.ca>
2023-01-11 16:54:59 -0500

    navbar and footer components created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavbarJohn />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <FooterJohn />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import NavbarJohn from "./components/navbarJohn.vue";
+import FooterJohn from "./components/footerJohn.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    NavbarJohn,
+    FooterJohn,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  overflow: auto;
+  height: 100vh;
+  background-color: #f7f7f7;
+  font-family: Helvetica, "Arial Narrow Bold", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/footerJohn.vue
@@ -0,0 +1,103 @@
+<template>
+  <footer class="container-fluid p-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-8 order-md-2 mb-3">
+          <ul class="footer-list">
+            <li class="col-12 col-sm-6 col-lg-4">
+              <a href="index.html">Home</a>
+            </li>
+            <li class="col-12 col-sm-6 col-lg-4"><a href="blog/">Blog</a></li>
+            <li class="col-12 col-sm-6 col-lg-4">Contact</li>
+            <li class="col-12 col-sm-6 col-lg-4">Accessibility</li>
+            <li class="col-12 col-sm-6 col-lg-4">Buying</li>
+            <li class="col-12 col-sm-6 col-lg-4">Selling</li>
+            <li class="col-12 col-sm-6 col-lg-4">About</li>
+            <li class="col-12 col-sm-6 col-lg-4">Your Mississauga</li>
+            <li class="col-12 col-sm-6 col-lg-4">Communities</li>
+          </ul>
+        </div>
+        <hr class="footerhr d-md-none mb-3 px-3" />
+        <div class="col-md-6 col-lg-4 order-md-1 contact-footer">
+          <div class="phone">
+            <a href="tel:416-902-1054">416-902-1054</a>
+          </div>
+          <div>Contact</div>
+          <h3>John Manucdoc</h3>
+          <div>Broker</div>
+          <div>RE/MAX Professionals Inc., Brokerage</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  Name: "FooterJohn",
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  float: left;
+  display: block;
+  padding-left: 20px;
+  color: #f7f7f7;
+  text-transform: uppercase;
+}
+footer {
+  background-color: #272727;
+  color: #f7f7f7;
+  text-align: left;
+}
+.footer-list {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+footer li {
+  text-transform: capitalize;
+  padding: 0;
+}
+.footerhr {
+  color: #c1202f;
+  border-width: 5px;
+  width: 100%;
+}
+.phone {
+  color: white;
+  font-weight: bold;
+}
+.phone a,
+.phone a:visited {
+  text-decoration: none;
+  color: white;
+}
+.phone:before {
+  content: "";
+  display: block;
+  background: url(../assets/images/icon_phone_white.svg) no-repeat;
+  width: 18px;
+  height: 18px;
+  float: left;
+  margin: 2px 1px;
+}
+.footer-list a {
+  color: #f7f7f7;
+  text-decoration: none;
+}
+@media screen and (min-width: 768px) {
+  .contact-footer {
+    border-right: 2px solid #c1202f;
+  }
+  .footer-list {
+    padding-left: 10px;
+  }
+}
+</style>
