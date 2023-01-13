615b436486ab58cb8a57a3a902542aa894c03b8a
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-01-13 10:58:08 -0500

    added john contact object data file

---
+++ src/js/johnContact.js
@@ -0,0 +1,10 @@
+const johnContact = [ {
+    johnName: "John Manucdoc",
+    johnNumber: "416-902-1054",
+    johnTwitter: "https://twitter.com/johnman10",
+    johnFacebook: "https://www.facebook.com/john.manucdoc",
+    johnLinkedin: "https://www.linkedin.com/in/johnmanucdoc/",
+
+} ];
+
+export default johnContact;
