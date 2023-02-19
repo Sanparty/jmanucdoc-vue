13b474f2ba334f996799dafb561cc05b5034794a
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-02-19 18:06:24 -0500

    updated john contact page

--- src/js/components/johnContact.js
+++ src/js/components/johnContact.js
@@ -3,11 +3,18 @@ const johnContact = {
     johnTitle: "Broker",
     johnCompany: "RE/MAX Professionals Inc., Brokerage",
     johnNumber: "416-902-1054",
+    johnNumberOffice: "416-232-9000",
     johnEmail: "john@remaxprofessionals.ca",
     johnTwitter: "https://twitter.com/johnman10",
     johnFacebook: "https://www.facebook.com/john.manucdoc",
     johnLinkedin: "https://www.linkedin.com/in/johnmanucdoc/",
-    johnMessage: "With you every step of the way"
+    johnMessage: "With you every step of the way",
+    johnAddress: {
+        johnStreet: "1 The East Mall Crescent, Third Floor",
+        johnCity: "Toronto",
+        johnProvince: "ON",
+        johnPostalCode: "M9B 6G8"
+    }
 
 } ;
 
