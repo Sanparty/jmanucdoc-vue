2858505ba9882c73f60c3b5a3c2e6b6384807d09
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-03-01 16:51:17 -0500

    updated buying container

--- src/buying/buyingContainer.vue
+++ src/buying/buyingContainer.vue
@@ -1,22 +1,70 @@
 <template>
-<div class="home-container container-fluid">
-   <div class="home-bio container-md mt-5">
-     <h1>{{ buyingInfo.title.title1 }}</h1>
-     <div v-html="buyingInfo.content.content1"></div>
-     <h1>{{ buyingInfo.title.title2 }}</h1>
-     <div v-html="buyingInfo.content.content"></div>
+<div class="home-container container-fluid p-0">
+  <div class="d-flex flex-wrap align-items-start">
+    <div class="col-12 col-lg-6">
+    <SubMainJohn 
+      :title="`Buying`"
+      :image="`url(${require('../assets/images/homeforsale.jpg')})`"
+      :altimage="`Home For Sale`"
+      :bgPosition="`center top`"
+      :bgSize="`cover`"
+      class="col-lg-6"
+      />
     </div>
+    <div class="col-12 col-lg-6">
+   <!-- <BuyingHome 
+   :pageInfo="buying"
+   /> -->
+
+ <BuyingHome 
+   :pageInfo="selectingAgent"
+   />
+   <!---
+   <BuyingHome 
+   :pageInfo="arrangeFinancing"
+  
+   />
+   <BuyingHome 
+   :pageInfo="viewingHomes"
+  
+   />
+   <BuyingHome 
+   :pageInfo="makingOffer"
+ 
+   />
+   <BuyingHome 
+   :pageInfo="closingSale"
+ 
+   /> -->
+  </div>
+  </div>
+    <div class="container-fluid">
+  <ContactJohn />
+  </div>
 </div>
 
 </template>
 
 <script>
-    import { buying } from "../js/components/buying"
+    import ContactJohn from "../components/contactJohn.vue"
+    import SubMainJohn from "@/components/subMainJohn.vue"
+    import BuyingHome from "../buying/buyingHome.vue"
+    import { buying, selectingAgent, arrangeFinancing, viewingHomes, makingOffer, closingSale } from "../js/components/buying"
     export default {
        name: "buyingContainer",
+       components: {
+        SubMainJohn,
+        BuyingHome,
+        ContactJohn,
+      },
       data() {      
         return {
-          buyingInfo: buying
+          buying: buying,
+          selectingAgent: selectingAgent,
+          arrangeFinancing: arrangeFinancing,
+          viewingHomes: viewingHomes,
+          makingOffer: makingOffer,
+          closingSale: closingSale
         };
       },
     }
@@ -26,6 +74,20 @@
 </script>
 
 <style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 .home-container {
     padding-top: 20px;
     background-color: #f7f7f7;
