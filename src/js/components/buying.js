12382b249fece53914ce739dfa7dae7a0d1a4b64
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-03-01 16:50:43 -0500

    added buying data

--- src/js/components/buying.js
+++ src/js/components/buying.js
@@ -21,5 +21,130 @@ const buying = {
         <p>Let’s work together to find your dream home!</p>`
     },
 }
-
-export  { buying }
+const selectingAgent = {
+    heading: "Selecting an Agent",
+    title: {
+        title1: "Do You Have a Personal Real Estate Agent?",
+        title2: "Your Real Estate Agent Works for You",
+        title3: "What to Look for in a Real Estate Agent",
+        title4: "Next Steps..."
+    },
+    content: {
+        content1: `<p>If you’re planning on buying a home, you may have already done some shopping around on your own. Perhaps you’ve checked out a few open houses or called the number on a For Sale sign to schedule a private viewing of that property.</p>
+        <p>Yes, you can do all of that on your own, if you have the time and expertise. However, statistics prove that you’ll be more successful in finding your dream home at the right price with your own personal real estate agent.</p>`,
+        content2: `<p>A real estate agent, especially one who knows the local market well (like I do), can help you find and buy your ideal home, quickly, and make the entire process go smoothly for you. During a viewing, your real estate agent will give you the inside scoop on that property, so you know the full story. Your real estate agent will also point out desirable features, and potential “issues”, that you might otherwise miss.</p>
+        <p>When it comes time to make an offer, your real estate agent will present and negotiate it on your behalf. If you don’t have your own real estate agent, you would have to deal directly with the seller’s agent, who is legally obliged to work in the seller’s best interests – not yours.</p>
+        <p>Bottom line: Your own personal real estate agent is there for you, every step of the way. At least, that’s how I work with my clients.</p>`,
+        content3: `<p>When buying a home, you want a real estate agent who…</p>
+        <ul>
+        <li>Takes the time to understand your needs and goals, so you have a true partner in your home buying journey.</li>
+        <li>Explains everything clearly and answers your questions, so you’re never in the dark.</li>
+        <li>Has expertise and a track record of success in the local market.</li>
+        <li>Knows how to find great properties in your price range.</li>
+        <li>Will alert you to new listings, quickly, so you get the jump on those opportunities.</li>
+        <li>Will advise you on the best price to offer for the home you want.</li>
+        <li>Will prepare and present the offer, and skillfully negotiate such things as price, conditions, closing dates, etc. so you get the best deal.</li>
+        <li>Will save you potentially thousands of dollars by ensuring you don’t overpay for the property you want.</li>
+        <li>Will recommend reputable home inspectors, movers and other professionals you may need.</li>
+        </ul>
+        <p>Not every real estate agent offers all of these services. My clients say that I do. They also tell me how much they appreciate my professionalism, expertise and accessibility.</p>`,
+        content4: `<p>The best way to select the right real estate agent, is to meet with him or her in person. Ask questions. See if there is a good fit.</p>
+        <p>To schedule an appointment with me, please call or email. If it’s more convenient for you, submit the form below and I’ll get back to you.</p>`,
+    },
+}
+const arrangeFinancing = {
+    heading: "Arrange Financing",
+    title: {
+        title1: "Get a Mortgage Pre-Approval",
+        title2: "How Much Home Can You Afford?",
+    },
+    content: {
+        content1: `<p>An important ingredient in a successful home search is knowing how much you can afford. You don’t want to fall in love with a property only to find out it’s outside your price range.</p>
+        <p>That’s why I recommend getting a Mortgage Pre-Approval. This is a document from your bank or other lender stating how much of a mortgage they are willing to give you. There are two advantages to getting this pre-approval:</p>
+        <ul>
+        <li>It gives you the peace-of-mind of knowing you can afford homes you view on the market, and won’t have problems arranging financing.</li>
+        <li>It’s a sign to sellers that you are a serious, prepared buyer – which gives you the edge when making an offer.</li>
+        </ul>
+        <p>You can arrange for a Mortgage Pre-Approval with your bank or other lender, or by working with a good mortgage broker. I’m well-connected in the local real estate scene, so if you want me to recommend a reputable broker or lender, please contact me.</p>
+        `,
+        content2: `<p>Banks and lenders use specific criteria to determine how much of a mortgage they’re willing to offer you. They look at your income, expenses, credit history and employment status, as well as the down payment you plan to make on a home. It may seem like a scary process, but it is actually straightforward. You can usually get a Mortgage Pre-Approval in a day. You’ll be glad that you did.</p>
+        <p>By knowing what you can afford, you can confidently shop within that price range. For example, if you qualify for a $250,000 mortgage, and you have another $100,000 available as a down payment, you can look for homes in the $350,000 range.</p>
+        <p>Have questions about arranging financing? Please submit the form below or call or email me.</p>`,
+    },
+}
+const viewingHomes = {
+    heading: "Viewing Homes",
+    title: {
+        title1: "The Home Shopping Process",
+        title2: "What to Look for when Viewing a Property",
+    },
+    content: {
+        content1: `<p>As your real estate agent, my most important job is to make the home shopping process pleasant and rewarding for you.</p>
+        <p>I do that in a number of ways.</p>
+        <p>I regularly recommend properties for viewing that closely match your needs and wants, and are within your price range. That spares you the frustration of visiting homes that aren’t suitable.</p>
+        <p>I make sure you don’t miss opportunities. When a desirable property suddenly comes on the market, you’re alerted.</p>
+        <p>I help you schedule viewings that fit your schedule, so there’s little if any inconvenience for you.</p>
+        <p>Before you view a property, I provide you with in-depth details on the home, so you can make an informed decision. This includes closing dates, market value, and more. Later, if you have questions, I get the answers for you.</p>
+        <p>I also give you the scoop on the neighbourhood, so you get a clear sense of what an area is like in terms of lifestyle, safety, schools, etc. You can count on me to steer you away from undesirable areas, and point out only those I suspect you’d like.</p>
+        <p>When you find a property you like, I give you my honest, professional recommendation as to how well the home meets your needs and wants. You won’t have to make such an important decision without my expert advice.</p>
+        <p>As your real estate agent, I’m committed to getting you into the right home, and I’m with you every step of the way.</p>
+        `,
+        content2: `<p>Here are a few questions and ideas to consider when viewing homes to help make the process easier and more productive:</p>
+        <ul>
+        <li>Does the home have your “needs” features (those features you must have)? (For example, number of bedrooms, closet space, kitchen size, etc.)</li>
+        <li>Does the home have any of your “wants” features (those features you’d like to have but would be willing to forgo)? (For example, large backyard deck, spacious family room, etc.)</li>
+        <li>What is the general condition of the home? (Are there signs that the owners have kept up with repairs and maintenance?)</li>
+        <li>Will the home need any major repairs, remodeling or renovations? Is there anything, such as the furnace, that may need to be replaced soon?</li>
+        <li>What do you like most about the home?</li>
+        <li>Is there anything about the home you don’t like? Can that be changed with a remodeling or renovation now or in the future?</li>
+        <li>Take a walk around the neighbourhood. Are the homes well-kept, indicating pride of ownership?</li>
+        <li>Do you like where the home is situated in the neighbourhood?</li>
+        <li>Does the neighbourhood have what you want? (For example, access to public transit, shopping, local parks, etc.)</li>
+        </ul>
+        <p>Viewing properties should never be a frustrating ordeal. It’s my job to help make the process easy and productive for you, so you can find the home you want.</p>
+        <p>Ready to get started? Call or email and let me know more about the kind of home you’re looking for. Or, submit the form below and I’ll get back to you.</p>
+        `,
+    },
+}
+const makingOffer = {
+    heading: "Making an Offer",
+    title: {
+        title1: "Preparing the Offer",
+        title2: "How the Process Works",
+    },
+    content: {
+        content1: `<p>When you view a home on the market that you really want, you can hardly wait to be handed the keys. In fact, you may worry that the property will somehow slip through your fingers. Don’t be. You’ve got an expert by your side. Me. My job, as your real estate agent, is to ensure you get that home – and at a price that works for you.</p>
+        <p>The first step is preparing the Offer. This is the document I present to the seller that basically states the price you’re offering, as well as any conditions.</p>
+        <p>The price we go in with is critical. You want it to reflect the true market value of the property so the seller will take your offer seriously. On the other hand, you don’t want to go in too high and risk overpaying for the home.</p>
+        <p>Don’t worry. I know how to prepare a winning offer and get you the property at the lowest possible price. In fact, I look after your interests throughout the entire process.</p>        
+        `,
+        content2: `<p>When you decide to make an offer on a home, here’s what typically happens:</p>
+        <ul>
+        <li>I’ll recommend a price that has the best chance of being accepted by the seller, without over-paying for the home.</li>
+        <li>Obviously, sellers prefer offers that have no conditions. However, I may suggest a condition, such as passing a professional home inspection, be included for your protection. We’ll talk about that.</li>
+        <li>I’ll prepare the necessary documents and present your offer to the seller and his or her real estate agent personally.</li>
+        <li>If the offer isn’t accepted, or if the seller makes a counter-offer, I’ll work with the seller’s agent, skillfully negotiating on your behalf to make a deal.</li>
+        <li>I’ll communicate with you throughout the entire process. You’re never in the dark when you work with me.</li>
+        <li>Once the seller agrees to a deal, I’ll help lead you through removal of any conditions and closing the deal. Then, the home is yours. Congratulations!</li>
+        </ul>
+        <p>I realize that making an offer can seem like the nail-biting part of the process. It doesn’t have to be. That’s the advantage of selecting a real estate agent who, like me, puts clients first. When you go after a property you really want, you can take comfort in knowing you have an expert on your side… someone who knows how to get that home for you.</p>
+        <p>Have questions about making an offer? Please submit the form below or call or email me.</p>        
+        `,
+    },
+}
+const closingSale = {
+    heading: "Closing the Sale",
+    title: {
+        title1: "Ensuring the Closing Goes Smoothly"
+    },
+    content: {
+        content1: `<p>There are few things I enjoy more than saying to a client, “You’ve got the home!” That means that the seller has accepted your offer, conditions have been waived, the closing date has been set, and you’ll soon be preparing for moving day.</p>
+        <p>Just because the purchase has been made doesn’t mean my work for you stops. On the contrary. There are a myriad of other things I’ll do to ensure the process goes smoothly.</p>
+        <p>For example, I’ll take care of many of the administrative details on your behalf. I’ll work with the seller’s agent, your lawyer, and your mortgage lender to ensure that all the necessary paperwork has been completed and submitted.</p>
+        <p>Don’t worry about finding a good real estate lawyer, insurance agent, moving company, etc. I’m well-connected in the local real estate scene and can personally recommend reputable professionals and companies to help you.</p>
+        <p>If at any time leading up to moving day you have a question, problem or concern, I’ll be there to help. You can count on me to be your real estate agent before, during and after you cross the threshold into your beautiful new home.</p>
+        <p>If you have questions about the home buying process, please feel free to call or email or submit the form below.</p>              
+        `
+    },
+}
+export  { buying, selectingAgent, arrangeFinancing, viewingHomes, makingOffer, closingSale }
